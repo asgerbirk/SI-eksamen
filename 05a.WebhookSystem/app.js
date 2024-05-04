6ec8a49f1ae6423b8fa2d9d361f51db465cdcc87
asgerbirk <asger.birk@gmail.com>
2024-05-04 14:39:11 +0200

    changes

--- 05a.WebhookSystem/app.js
+++ 05a.WebhookSystem/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { setupDb } from "./database.js";
 import bodyParser from "body-parser";
-import axios from "axios";
 
 const app = express();
 app.use(bodyParser.json());
