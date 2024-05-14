1ae3546c27ad892f80f220e15d403c192ad6272f
asgerbirk <asger.birk@gmail.com>
2024-05-14 18:02:19 +0200

    Auth

--- 08b.Database_migrate/app.js
+++ 08b.Database_migrate/app.js
@@ -15,39 +15,25 @@ const mongoClient = new MongoClient(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 });
 
-async function testDbConnection() {
+async function migrateData() {
   try {
-    const client = await pool.connect();
-    const result = await client.query("SELECT * FROM random_data");
-    client.release();
-    console.log(
-      "Database connection is successful. Retrieved data:",
-      result.rows
-    );
-  } catch (error) {
-    console.error("Database connection failed:", error);
-  }
+    // Connect to PostgreSQL
+    const pgClient = await pool.connect();
+    // Retrieve all data from a specific table in PostgreSQL
+    const pgData = await pgClient.query("SELECT * FROM random_data");
+    pgClient.release();
+    console.log("Data retrieved from PostgreSQL:", pgData.rows);
 
-  try {
+    // Connect to MongoDB
     await mongoClient.connect();
-    console.log("MongoDB connection is successful.");
-
     const db = mongoClient.db(process.env.MONGO_DB);
     const collection = db.collection(process.env.COLLECTION);
 
-    const sampleData = {
-      name: "John Doe",
-      age: 30,
-      email: "john.doe@example.com",
-    };
-    await collection.insertOne(sampleData);
-    console.log("Sample data inserted into MongoDB.");
-
-    // Retrieve the inserted data
-    const insertedData = await collection.findOne({ name: "John Doe" });
-    console.log("Retrieved data from MongoDB:", insertedData);
+    // Insert data into MongoDB
+    const insertResult = await collection.insertMany(pgData.rows);
+    console.log(insertResult);
   } catch (error) {
-    console.error("MongoDB connection failed:", error);
+    console.error("Migration failed:", error);
   } finally {
     await mongoClient.close();
   }
@@ -56,5 +42,5 @@ async function testDbConnection() {
 const PORT = process.env.PORT || 3000;
 app.listen(3000, () => {
   console.log(`Server is running on port ${PORT}`);
-  testDbConnection();
+  migrateData();
 });
